37da2e48d15952054294e5450ad73141342c7e85
13mcc1005 <amitdb5550@gmail.com>
2016-01-21 15:40:57 +0530

    url

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, '54.208.23.139');
+console.log('Server running at http://54.208.23.139:8080/');
